51637046ae62b397a2da00d29dd5eec5bc846acf
TranPhuocMyToan <67469007+mytoandeptrai@users.noreply.github.com>
2022-08-17 00:30:03 +0700

    update peer

--- client/src/RoomScreen.js
+++ client/src/RoomScreen.js
@@ -1,103 +1,138 @@
-import React, { useEffect, useRef, useState } from "react";
-import { useParams } from "react-router-dom";
-import io from "socket.io-client";
+import React, { useEffect, useRef, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import io from 'socket.io-client';
 
 function generateUserName(length) {
-  var result = "";
-  var characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  var charactersLength = characters.length;
-  for (var i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
+   var result = '';
+   var characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   var charactersLength = characters.length;
+   for (var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
 }
 
 const RoomScreen = (props) => {
-  const { roomId } = useParams();
-  const socket = useRef();
-  const messageRef = useRef();
-  const [usersInRoom, setUsersInRoom] = useState([]);
-  const [messages, setMessages] = useState([]);
-  const [currentUser, setCurrentUser] = useState(null);
-  useEffect(() => {
-    socket.current = io.connect("http://localhost:8000");
+   const { roomId } = useParams();
+   const socket = useRef();
+   const messageRef = useRef();
+   const [usersInRoom, setUsersInRoom] = useState([]);
+   const [messages, setMessages] = useState([]);
+   const [currentUser, setCurrentUser] = useState(null);
+   useEffect(() => {
+      socket.current = io.connect('http://localhost:8000');
 
-    const user = generateUserName(5);
-    setCurrentUser(user);
+      const user = generateUserName(5);
+      setCurrentUser(user);
 
-    socket.current.emit("user-joined-room", {
-      userName: user,
-      roomId,
-    });
-
-    socket.current.on("fulled-room", () => {
-      alert("Full room rồi");
-      return;
-    });
-
-    socket.current.on("userInRoom", (userInThisRoom) => {
-      setUsersInRoom(userInThisRoom);
-    });
-
-    socket.current.on("userLeft", ({ leavedUser, existedUserInRoom }) => {
-      const onlineUserInRoom = existedUserInRoom.filter(
-        (el) => el.userId !== leavedUser.userId
-      );
-      setUsersInRoom(onlineUserInRoom);
-      console.log(`User ${leavedUser.userName} left the room`);
-    });
-
-    socket.current.on("sendMessageToClient", (payload) => {
-      console.log(payload);
-      setMessages((prev) => [...prev, payload]);
-    });
-  }, []);
-
-  const sendMessage = (e) => {
-    e.preventDefault();
-    if (socket.current) {
-      socket.current.emit("sendMessageToServer", {
-        roomId,
-        message: messageRef.current.value,
-        currentUser
+      socket.current.emit('user-joined-room', {
+         userName: user,
+         roomId,
       });
-      messageRef.current.value = "";
-    }
-  };
 
-  return (
-    <div>
-      RoomScreen {usersInRoom.length} users in room
-      <div className="room row">
-        <div className="videoCall"></div>
-        <div className="chat row">
-          <div className="chat__header">
-            <h6>Chat</h6>
-          </div>
+      socket.current.emit('message-joined-room', {
+         roomId,
+      });
 
-          <div className="chat__msg-container">
-            <ul className="messages">
-              {messages.map((message, index) => (
-                <p key={index}>
-                  ({message.userName}):{message.message}
-                </p>
-              ))}
-            </ul>
-          </div>
+      socket.current.on('fulled-room', () => {
+         alert('Full room rồi');
+         return;
+      });
 
-          <form onSubmit={sendMessage} className="chat__msg-send-container">
-            <input
-              ref={messageRef}
-              type="text"
-              placeholder="Type message here..."
-            />
-            <i onClick={sendMessage} className="fa fa-paper-plane" />
-          </form>
-        </div>
+      socket.current.on('userInRoom', (userInThisRoom) => {
+         setUsersInRoom(userInThisRoom);
+         const peers = [];
+         userInThisRoom.forEach(user => {
+          
+         })
+      });
+
+      socket.current.on('messageInRoom', (messages) => {
+         if (messages) {
+            setMessages(messages);
+         }
+      });
+
+      socket.current.on('userLeft', ({ leavedUser, existedUserInRoom }) => {
+         const onlineUserInRoom = existedUserInRoom.filter(
+            (el) => el.userId !== leavedUser.userId
+         );
+         setUsersInRoom(onlineUserInRoom);
+         console.log(`User ${leavedUser.userName} left the room`);
+      });
+
+      socket.current.on('sendMessageToClient', (payload) => {
+         console.log(payload);
+         setMessages((prev) => [...prev, payload]);
+      });
+   }, []);
+
+   const sendMessage = (e) => {
+      e.preventDefault();
+      if (socket.current) {
+         socket.current.emit('sendMessageToServer', {
+            roomId,
+            message: messageRef.current.value,
+            currentUser,
+         });
+         messageRef.current.value = '';
+      }
+   };
+
+   const createPeer = (userIdToSendSignal, callerId, stream) => {
+      const peer = new Peer({
+         initiator: true,
+         trickle: false,
+         stream,
+      });
+
+      peer.on('signal', (signal) => {
+         socket.current.emit('sendingSignal', {
+            userIdToSendSignal,
+            callerId,
+            signal,
+         });
+      });
+
+      return peer;
+   };
+
+   return (
+      <div>
+         RoomScreen {usersInRoom.length} users in room
+         <div className='room row'>
+            <div className='videoCall'></div>
+            <div className='chat row'>
+               <div className='chat__header'>
+                  <h6>Chat</h6>
+               </div>
+
+               <div className='chat__msg-container'>
+                  <ul className='messages'>
+                     {messages.map((message, index) => (
+                        <p key={index}>
+                           ({message.userName}):{message.message}
+                        </p>
+                     ))}
+                  </ul>
+               </div>
+
+               <form
+                  onSubmit={sendMessage}
+                  className='chat__msg-send-container'
+               >
+                  <input
+                     ref={messageRef}
+                     type='text'
+                     placeholder='Type message here...'
+                  />
+                  <i onClick={sendMessage} className='fa fa-paper-plane' />
+               </form>
+            </div>
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default RoomScreen;
--- server.js
+++ server.js
@@ -9,9 +9,10 @@ const users = {};
 
 const socketToRoom = {};
 const usersInRoom = {};
+const messagesInRoom = {};
 
 io.on('connection', (socket) => {
-    /** 
+   /** 
     if (!users[socket.id]) {
         users[socket.id] = socket.id;
     }
@@ -33,67 +34,87 @@ io.on('connection', (socket) => {
         io.to(data.to).emit('callAccepted', data.signal);
     });
     */
-    console.log('Some one joined socketId: ' + socket.id);
-    socket.on('user-joined-room', ({ userName, roomId }) => {
-        const currentUser = socket.id;
-        if (usersInRoom[roomId]) {
-            if (usersInRoom[roomId].length === 4) {
-                socket.emit('fulled-room');
-                return;
-            }
-            usersInRoom[roomId].push({ userName, userId: currentUser });
-        } else {
-            usersInRoom[roomId] = [{ userName, userId: currentUser }];
-        }
-        socketToRoom[socket.id] = roomId;
-
-        /** sending all socket id already joined user in this room */
-        io.sockets.emit('userInRoom', usersInRoom[roomId]);
-    });
-
-    socket.on('disconnect', () => {
-        /** get User Id who leave room */
-        const leaveRoomUserId = socket.id;
-        /** get the room user joined */
-        const roomId = socketToRoom[leaveRoomUserId];
-        let socketsIdConnectedToRoom = usersInRoom[roomId];
-        let leavedUser = null;
-        if (socketsIdConnectedToRoom) {
-            /** get User who leave room */
-            leavedUser = socketsIdConnectedToRoom.find(
-                (el) => el.userId === leaveRoomUserId
-            );
-            /** remove user from room */
-            socketsIdConnectedToRoom = socketsIdConnectedToRoom.filter(
-                (el) => el.userId !== leaveRoomUserId
-            );
-
-            /** send all the rest of people who are already in room */
-            socketsIdConnectedToRoom.forEach((user) => {
-                io.to(user.userId).emit('userLeft', {
-                    leavedUser,
-                    existedUserInRoom: usersInRoom[roomId],
-                });
-            });
-
-            usersInRoom[roomId] = socketsIdConnectedToRoom;
-        }
-
-        // io.sockets.broadcast.emit('userLeft', leaveUser);
-    });
-
-    socket.on('sendMessageToServer', ({roomId, message, currentUser}) => {
-      const currentUsersInRoom = usersInRoom[roomId];
-      if(currentUsersInRoom){
-
-        currentUsersInRoom.forEach((user) => {
-          io.to(user.userId).emit('sendMessageToClient', {
-            message,
-            userName: currentUser,
-          })
-        })
+   console.log('Some one joined socketId: ' + socket.id);
+   socket.on('user-joined-room', ({ userName, roomId }) => {
+      const currentUser = socket.id;
+      if (usersInRoom[roomId]) {
+         if (usersInRoom[roomId].length === 4) {
+            socket.emit('fulled-room');
+            return;
+         }
+         usersInRoom[roomId].push({ userName, userId: currentUser });
+      } else {
+         usersInRoom[roomId] = [{ userName, userId: currentUser }];
       }
-    })
+
+      if (!messagesInRoom[roomId]) {
+         messagesInRoom[roomId] = [];
+      }
+
+      socketToRoom[socket.id] = roomId;
+
+      socket.join(roomId); //for message
+      /** sending all socket id already joined user in this room */
+      io.sockets.emit('userInRoom', usersInRoom[roomId]);
+   });
+
+   socket.on('message-joined-room', ({ roomId }) => {
+      const messageInRoom = messagesInRoom[roomId];
+      io.to(roomId).emit('messageInRoom', messageInRoom);
+   });
+
+   socket.on('disconnect', () => {
+      /** get User Id who leave room */
+      const leaveRoomUserId = socket.id;
+      /** get the room user joined */
+      const roomId = socketToRoom[leaveRoomUserId];
+      let socketsIdConnectedToRoom = usersInRoom[roomId];
+      let leavedUser = null;
+      if (socketsIdConnectedToRoom) {
+         /** get User who leave room */
+         leavedUser = socketsIdConnectedToRoom.find(
+            (el) => el.userId === leaveRoomUserId
+         );
+         /** remove user from room */
+         socketsIdConnectedToRoom = socketsIdConnectedToRoom.filter(
+            (el) => el.userId !== leaveRoomUserId
+         );
+
+         /** send all the rest of people who are already in room */
+         socketsIdConnectedToRoom.forEach((user) => {
+            io.to(user.userId).emit('userLeft', {
+               leavedUser,
+               existedUserInRoom: usersInRoom[roomId],
+            });
+         });
+
+         usersInRoom[roomId] = socketsIdConnectedToRoom;
+      }
+
+      socket.leave(roomId); //for message group(socket)
+      // io.sockets.broadcast.emit('userLeft', leaveUser);
+   });
+
+   socket.on('sendMessageToServer', ({ roomId, message, currentUser }) => {
+      const currentUsersInRoom = usersInRoom[roomId];
+      let currentMessagesInRoom = messagesInRoom[roomId];
+      if (currentMessagesInRoom) {
+         currentMessagesInRoom = [
+            ...currentMessagesInRoom,
+            { message, userName: currentUser },
+         ];
+         messagesInRoom[roomId] = currentMessagesInRoom;
+      }
+
+      if (currentUsersInRoom) {
+         currentUsersInRoom.forEach((user) => {
+            io.to(user.userId).emit('sendMessageToClient', {
+               message,
+               userName: currentUser,
+            });
+         });
+      }
+   });
 });
 
 server.listen(8000, () => console.log('server is running on port 8000'));
